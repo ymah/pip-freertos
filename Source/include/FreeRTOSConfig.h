660ea548a7ee5d82973902197fc9ed1d5268e2c7
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2018-06-22 08:24:33 +0200

    Correcting stack issue

--- Source/include/FreeRTOSConfig.h
+++ Source/include/FreeRTOSConfig.h
@@ -151,14 +151,14 @@
 
 #define configCPU_CLOCK_HZ						( 400000000UL )
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION	1
-#define configMINIMAL_STACK_SIZE				( 250 )
+#define configMINIMAL_STACK_SIZE				( 124 )
 #define configUSE_TICKLESS_IDLE					0
 #define configTICK_RATE_HZ						( ( TickType_t ) 2000 )
 #define configUSE_PREEMPTION					1
 #define configUSE_IDLE_HOOK						0
 #define configUSE_TICK_HOOK						0
 #define configMAX_PRIORITIES					( 15 )
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 10 * 1024 ) )
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 1000 * 1024 ) )
 #define configMAX_TASK_NAME_LEN					( 10 )
 #define configUSE_TRACE_FACILITY				0
 #define configUSE_16_BIT_TICKS					0
